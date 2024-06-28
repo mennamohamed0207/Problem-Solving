2e2b8f4923fb63939a6dd22cdcb12a047d209b60
mennamohamed0207 <menamohamed0207@gmail.com>
2024-06-28 07:58:56 +0300

    Generic Solution for isomorphic problem is added

--- is_isomorphic.cpp
+++ is_isomorphic.cpp
@@ -1,3 +1,33 @@
+/*
+Given two strings s and t, determine if they are isomorphic.
+
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+
+ 
+
+Example 1:
+
+Input: s = "egg", t = "add"
+Output: true
+Example 2:
+
+Input: s = "foo", t = "bar"
+Output: false
+Example 3:
+
+Input: s = "paper", t = "title"
+Output: true
+ 
+
+Constraints:
+
+1 <= s.length <= 5 * 104
+t.length == s.length
+s and t consist of any valid ascii character.
+*/
+
 class Solution {
 public:
     bool is_number(const std::string& s) {
@@ -51,4 +81,45 @@ public:
 
         return true;}
     }
+};
+//Second Generic Solution
+class Solution {
+public:
+    bool is_number(const std::string& s) {
+        std::string::const_iterator it = s.begin();
+        while (it != s.end() && std::isdigit(*it))
+            ++it;
+        return !s.empty() && it == s.end();
+    }
+    bool isIsomorphic(string s, string t) {
+        if (s.length() != t.length())
+            return false;
+
+        std::unordered_map<char, char> map_s_to_t;
+        std::unordered_map<char, char> map_t_to_s;
+
+        for (int i = 0; i < s.length(); ++i) {
+            char char_s = s[i];
+            char char_t = t[i];
+
+            // Check mapping from s to t
+            if (map_s_to_t.find(char_s) == map_s_to_t.end()) {
+                map_s_to_t[char_s] = char_t;
+            } else {
+                if (map_s_to_t[char_s] != char_t)
+                    return false;
+            }
+
+            // Check mapping from t to s
+            if (map_t_to_s.find(char_t) == map_t_to_s.end()) {
+                map_t_to_s[char_t] = char_s;
+            } else {
+                if (map_t_to_s[char_t] != char_s)
+                    return false;
+            }
+        }
+
+        return true;
+    }
+    
 };
