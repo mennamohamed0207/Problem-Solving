09639a44fa3e199ae2ede4e34f3d08d5b063663b
mennamohamed0207 <menamohamed0207@gmail.com>
2024-08-25 23:54:31 +0300

    adding hard palidrom

---
+++ FindClosestPalidrom.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalidrom(int num)
+    {
+        string str=to_string(num);
+        int j=0;
+        for(int i=0;str.length()/2;i++)
+        {
+            
+        }
+    }
+    string nearestPalindromic(string n) {
+        
+    }
+};
