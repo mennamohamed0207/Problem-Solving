0999d34aef01e03ef06b1e097297fb87d9ef337a
Menna Mohamed Abdelbaset <90017398+mennamohamed0207@users.noreply.github.com>
2025-01-01 19:17:37 +0200

    [1422] [Time Beats: 100.00%] [Memory Beats: 11.67%] - LeetPush

--- 1422-Maximum-Score-After-Splitting-a-String.cpp
+++ 1422-Maximum-Score-After-Splitting-a-String.cpp
@@ -8,21 +8,16 @@ public:
             if (s[i] == '1')
                 oneNum++;
         }
-        bool allOnes = true;
-        bool allZeros = true;
+
         for (int i = 0; i < s.length()-1; i++) {
             if (s[i] == '0') {
-                allOnes = false;
                 zeroNum++;
             } else if (s[i] == '1') {
-                allZeros=false;
                 oneNum--;
             }
                 scores[i] = zeroNum + oneNum;
         }
         int max = *std::max_element(scores.begin(), scores.end());
-        // if (allOnes||allZeros)
-        //     max = s.length() - 1;
         return max;
     }
 };
