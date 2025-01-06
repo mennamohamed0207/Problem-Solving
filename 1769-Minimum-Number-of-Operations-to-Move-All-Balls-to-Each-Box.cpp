a4191cda928b9862aae00e5099f4c5d911445294
Menna Mohamed Abdelbaset <90017398+mennamohamed0207@users.noreply.github.com>
2025-01-06 03:32:36 +0200

    [1769] [Time Beats: 43.98%] [Memory Beats: 50.97%] - LeetPush

--- 1769-Minimum-Number-of-Operations-to-Move-All-Balls-to-Each-Box.cpp
+++ 1769-Minimum-Number-of-Operations-to-Move-All-Balls-to-Each-Box.cpp
@@ -4,13 +4,12 @@ public:
         vector<int> answers(boxes.length());
         for (int i = 0; i < boxes.length(); i++) {
             int sum = 0;
-            for (int j = 0; j < boxes.length(); j++) {
-                if (boxes[j] == '1') {
-                    sum += abs(j - i);
-                    // boxes[j]='0';
+            if (boxes[i] == '1') {
+                for (int j = 0; j < boxes.length(); j++) {
+                     answers[j] += abs(j - i);
                 }
+            // answers[i] = sum;
             }
-            answers[i] = sum;
         }
         return answers;
     }
