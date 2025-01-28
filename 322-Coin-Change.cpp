0580159815cdea3149bdd1857edee3face2522b2
Menna Mohamed Abdelbaset <90017398+mennamohamed0207@users.noreply.github.com>
2025-01-28 18:11:35 +0200

    [322] [Time Beats: 69.16%] [Memory Beats: 58.41%] - LeetPush

--- 322-Coin-Change.cpp
+++ 322-Coin-Change.cpp
@@ -1,27 +1,15 @@
 class Solution {
 public:
-    unordered_map<int,int>dp;
-    int dfs(vector<int>& coins, int amount) {
-        if(amount==0)
-        return 0;
-        if(dp.find(amount)!=dp.end())
-        return dp[amount];
-        int res = INT_MAX;
-        for(int i=0;i<coins.size();i++)
-        {
-            if(amount-coins[i]>=0)
-            {
-                int result = dfs( coins,amount - coins[i]);
-                if (result != INT_MAX) {
-                    res = min(res, 1 + result);
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1, amount + 1);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.size(); j++) {
+                if (coins[j] <= i) {
+                    dp[i] = min(dp[i], dp[i - coins[j]] + 1);
                 }
             }
         }
-        dp[amount]=res;
-        return res;
-    }
-    int coinChange(vector<int>& coins, int amount) {
-        int minCoins = dfs( coins,amount);
-        return minCoins == INT_MAX ? -1 : minCoins;
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 };
