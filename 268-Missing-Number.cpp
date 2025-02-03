563040413ec4a9d9d1e5e7811181e954e6233d1d
Menna Mohamed Abdelbaset <90017398+mennamohamed0207@users.noreply.github.com>
2025-02-03 10:01:25 +0200

    [268] [Time Beats: 11.07%] [Memory Beats: 5.02%] - LeetPush

--- 268-Missing-Number.cpp
+++ 268-Missing-Number.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        
-        int n=nums.size();
-        if(n==1&&nums[0]==1) return 0;
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]!=i)
-            return i;
+        unordered_set<int> num_set(nums.begin(), nums.end());
+
+        int n = nums.size();
+        if (n == 1 && nums[0] == 1)
+            return 0;
+        for (int i = 0; i < n; i++) {
+           if(num_set.find(i)==num_set.end())
+                return i;
         }
         return n;
     }
