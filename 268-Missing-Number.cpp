dc1f5cc1f2901063866dfd28a6db0f8105e2eb1b
Menna Mohamed Abdelbaset <90017398+mennamohamed0207@users.noreply.github.com>
2025-02-03 09:59:46 +0200

    [268] [Time Beats: 18.78%] [Memory Beats: 70.78%] - LeetPush

---
+++ 268-Missing-Number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        
+        int n=nums.size();
+        if(n==1&&nums[0]==1) return 0;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n;i++)
+        {
+            if(nums[0]!=0)
+            return 0;
+            if(i<n-1&&nums[i+1]-nums[i]>1)
+            return nums[i+1]-1;
+        }
+        return n;
+    }
+};
