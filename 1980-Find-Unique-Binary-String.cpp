e826f548afb65c1cc8b78cf9a36911c6ea1c25bb
Menna Mohamed Abdelbaset <90017398+mennamohamed0207@users.noreply.github.com>
2025-02-20 19:39:47 +0200

    [1980] [Time Beats: 100.00%] [Memory Beats: 94.35%] - LeetPush

--- 1980-Find-Unique-Binary-String.cpp
+++ 1980-Find-Unique-Binary-String.cpp
@@ -1,34 +1,10 @@
 class Solution {
 public:
     string findDifferentBinaryString(vector<string>& nums) {
-     vector<string>combinations;
-            //  string cur(nums.size(), '0');
-     dfs("",0,combinations,nums[0].length());
-     sort(nums.begin(),nums.end());
-        cout<<"nums"<<endl;
-     for(int i=0;i<nums.size();i++)
-     {
-        // cout<<nums[i]<<" ";
-        if(combinations[i]!=nums[i])
-        return combinations[i];
-     }   
-     if(nums.size()< combinations.size())
-     {
-        return combinations[nums.size()];
-     }
-     return "";
-    }
-    void dfs(string s,int i,vector<string>&combinations,int n)
-    {
-        if(i==n)
-        {
-            combinations.push_back(s);
-            // cout<<s<<" ";
-            return ;
-
+        string res;
+        for (int i = 0; i < nums.size(); i++) {
+            res += (nums[i][i] == '0') ? '1' : '0';
         }
-        dfs(s+"0",i+1,combinations,n);
-        dfs(s+"1",i+1,combinations,n);
-
+        return res;
     }
 };
